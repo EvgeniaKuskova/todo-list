06ecbc86a98ab0587d18e66ed9296e480995b33c
AnastasiaKostareva <ankostareva08@gmail.com>
2025-05-13 18:14:32 +0500

    4

--- index.js
+++ index.js
@@ -32,17 +32,21 @@
 
 class Component {
   constructor() {
+    this._domNode = null;
   }
 
   getDomNode() {
-    this._domNode = this.render();
+    if (!this._domNode) {
+      this._domNode = this.render();
+    }
     return this._domNode;
   }
 
   update() {
-    const newTodo = this.render();
-    document.body.appendChild(newTodo)
-    this._domNode = newTodo;
+    const oldNode = this._domNode;
+    const newNode = this.render();
+    oldNode.parentNode.replaceChild(newNode, oldNode);
+    this._domNode = newNode;
   }
 }
 
@@ -50,64 +54,99 @@ class TodoList extends Component {
   constructor() {
     super();
     this.state = {
-      inputText : "",
+      inputText: "",
       todo: [
         {id: 1, text: "Сделать домашку", completed: false},
         {id: 2, text: "Сделать практику", completed: false},
         {id: 3, text: "Пойти домой", completed: false}
       ],
-      lastId : 3
-    }
+      lastId: 3
+    };
+    
+    this.onAddTask = this.onAddTask.bind(this);
+    this.onAddInputChange = this.onAddInputChange.bind(this);
+    this.onToggleComplete = this.onToggleComplete.bind(this);
+    this.onDeleteTask = this.onDeleteTask.bind(this);
   }
 
   render() {
     return createElement("div", { class: "todo-list" }, [
       createElement("h1", {}, "TODO List"),
       createElement(
-          "div",
-          { class: "add-todo" },
-          [
-            createElement("input", {
-              id: "new-todo",
-              type: "text",
-              placeholder: "Задание",
-              value: this.state.inputText,
-            }, null, {
-              change: this.onAddInputChange.bind(this),
-            }),
-            createElement("button", { id: "add-btn" }, "+", {click: this.onAddTask.bind(this)}),
-          ],
+        "div",
+        { class: "add-todo" },
+        [
+          createElement("input", {
+            id: "new-todo",
+            type: "text",
+            placeholder: "Задание",
+            value: this.state.inputText,
+          }),
+          createElement("button", { id: "add-btn" }, "+"),
+        ],
+        {
+          input: (e) => this.onAddInputChange(e),
+          click: (e) => e.target.id === "add-btn" && this.onAddTask(),
+        }
       ),
       createElement(
-          "ul",
-          { id: "todos" },
-          this.state.todo.map((todo) =>
-              createElement("li", { key: todo.id }, [
-                createElement("input", {
-                  type: "checkbox",
-                  checked: todo.completed,
-                }, null, ),
-                createElement("label", {}, todo.text),
-                createElement("button", {}, "🗑️", ),
-              ])
+        "ul",
+        { id: "todos" },
+        this.state.todo.map((todo) =>
+          createElement(
+            "li", 
+            { 
+              key: todo.id,
+              style: `color: ${todo.completed ? "gray" : "black"}; text-decoration: ${todo.completed ? "line-through" : "none"}`
+            }, 
+            [
+              createElement("input", {
+                type: "checkbox",
+                checked: todo.completed,
+                "data-id": todo.id,
+              }),
+              createElement("label", {}, todo.text),
+              createElement("button", { "data-id": todo.id }, "🗑️"),
+            ],
+            {
+              change: (e) => e.target.type === "checkbox" && this.onToggleComplete(todo.id),
+              click: (e) => e.target.textContent === "🗑️" && this.onDeleteTask(todo.id),
+            }
           )
+        )
       ),
     ]);
   }
 
   onAddTask() {
-    if (this.state.inputText.trim()){
-      this.state.todo.push({id: this.state.lastId + 1, text : this.state.inputText, completed : false});
+    if (this.state.inputText.trim()) {
+      this.state.todo.push({
+        id: this.state.lastId + 1,
+        text: this.state.inputText,
+        completed: false
+      });
       this.state.inputText = "";
       this.state.lastId += 1;
       this.update();
     }
-  };
+  }
 
   onAddInputChange(event) {
     this.state.inputText = event.target.value;
+  }
+
+  onToggleComplete(id) {
+    const todo = this.state.todo.find(item => item.id === id);
+    if (todo) {
+      todo.completed = !todo.completed;
+      this.update();
+    }
+  }
+
+  onDeleteTask(id) {
+    this.state.todo = this.state.todo.filter(item => item.id !== id);
     this.update();
-  };
+  }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
